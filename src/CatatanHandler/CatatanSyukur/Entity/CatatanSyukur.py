fd4a7c9dfcb0ae5c2f724345dae85d91e07c6e1c
Haziq <haziqabiyyu@gmail.com>
2023-04-02 18:22:46 +0700

    Add methods in CatatanSyukur

--- src/CatatanHandler/CatatanSyukur/Entity/CatatanSyukur.py
+++ src/CatatanHandler/CatatanSyukur/Entity/CatatanSyukur.py
@@ -10,5 +10,58 @@ class CatatanSyukur:
     
     def getTanggal(self):
         return self.tanggal
+
+    def getAllSyukur():
+        getAllQuery = QSqlQuery()
+        getAllQuery.exec("SELECT * FROM catatanSyukur")
+        allSyukur = []
+        while getAllQuery.next():
+            content = CatatanSyukur(getAllQuery.value(0), getAllQuery.value(1))
+            allSyukur.append(content)
+        return allSyukur
+
+    def save(self):
+        saveQuery = QSqlQuery()
+        saveQuery.prepare(
+            """
+            INSERT INTO catatanSyukur VALUES (?,?)
+            """
+        )
+        saveQuery.addBindValue(self.tanggal)
+        saveQuery.addBindValue(self.syukur)
+        saveQuery.exec()
+        return self.getAllSyukur()
     
-    #TODO: save, edit, delete, getAllSyukur
+    def edit(self, syukurLama):
+        editQuery = QSqlQuery()
+        editQuery.prepare(
+            """
+            UPDATE catatanSyukur
+            SET
+                tanggal = ?
+                syukur = ?
+            WHERE tanggal = ? AND syukur = ?
+            """
+        )
+        editQuery.addBindValue(self.tanggal)
+        editQuery.addBindValue(self.syukur)
+        editQuery.addBindValue(syukurLama.tanggal)
+        editQuery.addBindValue(syukurLama.syukur)
+        editQuery.exec()
+        return self.getAllSyukur()
+
+    def delete(self):
+        deleteQuery = QSqlQuery()
+        deleteQuery.prepare(
+            """
+            DELETE FROM catatanSyukur
+            WHERE tanggal = ? AND syukur = ?
+            """
+        )
+        deleteQuery.addBindValue(self.tanggal)
+        deleteQuery.addBindValue(self.syukur)
+        deleteQuery.exec()
+        return self.getAllSyukur()
+
+
+
